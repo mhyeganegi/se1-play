d141db23330bb3981f660697e437e4870a2896f8
mhyeganegi <vlzm6341@bht-berlin.de>
2026-05-18 15:25:59 +0200

    findSums() complete

--- src/main/numbers/NumbersImpl.java
+++ src/main/numbers/NumbersImpl.java
@@ -3,6 +3,10 @@ package numbers;
 import java.util.List;
 import java.util.Set;
 
+import java.util.ArrayList;
+
+import java.util.LinkedHashSet;
+
 class NumbersImpl implements Numbers {
 
     @Override
@@ -51,27 +55,76 @@ public long sumRecursive(int[] numbers, int i) {
 }
 
     @Override
-    public int findFirst(int[] numbers, int x) {
-        return -1;
+public int findFirst(int[] numbers, int x) {
+    if (numbers == null) {
+        throw new IllegalArgumentException("illegal argument: null");
     }
 
-    @Override
-    public int findLast(int[] numbers, int x) {
-        return -1;
+    for (int i = 0; i < numbers.length; i++) {
+        if (numbers[i] == x) {
+            return i;
+        }
     }
 
-    @Override
-    public List<Integer> findAll(int[] numbers, int x) {
-        return List.of();
+    return -1;
+}
+
+@Override
+public int findLast(int[] numbers, int x) {
+    if (numbers == null) {
+        throw new IllegalArgumentException("illegal argument: null");
     }
 
-    @Override
-    public Set<Pair> findSums(int[] numbers, int sum) {
-        return Set.of();
+    for (int i = numbers.length - 1; i >= 0; i--) {
+        if (numbers[i] == x) {
+            return i;
+        }
     }
 
-    @Override
-    public Set<Set<Integer>> findAllSums(int[] numbers, int sum) {
-        return Set.of();
+    return -1;
+}
+
+@Override
+public List<Integer> findAll(int[] numbers, int x) {
+    if (numbers == null) {
+        throw new IllegalArgumentException("illegal argument: null");
     }
+
+    List<Integer> indices = new ArrayList<>();
+
+    for (int i = 0; i < numbers.length; i++) {
+        if (numbers[i] == x) {
+            indices.add(i);
+        }
+    }
+
+    return indices;
+}
+@Override
+public Set<Pair> findSums(int[] numbers, int sum) {
+    if (numbers == null) {
+        throw new IllegalArgumentException("illegal argument: null");
+    }
+
+    Set<Pair> result = new LinkedHashSet<>();
+
+    for (int i = 0; i < numbers.length; i++) {
+        for (int j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == sum) {
+
+                int a = Math.min(numbers[i], numbers[j]);
+                int b = Math.max(numbers[i], numbers[j]);
+
+                result.add(new Pair(a, b));
+            }
+        }
+    }
+
+    return result;
+}
+
+@Override
+public Set<Set<Integer>> findAllSums(int[] numbers, int sum) {
+    return Set.of();
+}
 }
